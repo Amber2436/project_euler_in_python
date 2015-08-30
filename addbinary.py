3cb1617e26179aa9f2bb2a3e0931d85217ab72e6
Amber2436 <xysfyq@163.com>
2015-08-30 10:23:49 -0400

    finally

--- addbinary.py
+++ addbinary.py
@@ -1,3 +1,16 @@
 __author__ = 'xueyishu'
 
 
+
+def addBinary(a, b):
+	summ = 0
+	for i in range(len(a)):
+		summ += 2 ** i * int(a[::-1][i])
+	for j in range(len(b)):
+		summ += 2 ** j * int(b[::-1][j])
+	binary = ""
+	while summ:
+		binary += str(summ % 2)
+		summ /= 2
+	print binary[::-1]
+
